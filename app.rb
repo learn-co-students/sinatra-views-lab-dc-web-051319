8ea9c11dd5609185401bbd6dea87712fa4815716
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 16:40:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/goodbye' do
+	 
+		erb :goodbye 
+	end
+		get '/hello' do
+		erb :hello 
+	end
+		get '/date' do
+		  time = Time.new
+		  @time = time.strftime("%A, %B %d, %Y")
+		erb :date 
 	end
 
 
