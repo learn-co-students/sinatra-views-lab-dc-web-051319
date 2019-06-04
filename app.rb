1ccbbea0245e693086d73a1a0bbf64e9959e54e9
Your Name <you@example.com>
2019-06-04 09:09:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
+require 'date'
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
+	get '/hello' do
+		erb :hello
+	end
 
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
 
+	get '/date' do
+		@days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+		@months = %w(January February March April May June July August September October November December)
+		@currentTime = Time.now
+		erb :date
+	end
 end
